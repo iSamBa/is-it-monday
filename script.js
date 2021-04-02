6e09caadd347a49fc1f772679389900888892136
iSamBa <bahou.aissam@gmail.com>
2021-04-02 15:21:25 +0200

    add functional script

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello");
+const answer = document.getElementById("answer");
+
+const options = { weekday: "long" };
+const today = new Intl.DateTimeFormat("en-US", options).format(new Date());
+
+if (today === "Monday") {
+  answer.innerHTML = `<span style="font-style: normal; color : green">YES</span><br>Frehti daba ? Tfertih`;
+} else {
+  answer.innerHTML = `<span style="font-style: normal; color : red">NO</span><br>Lyoum ${today}, yallah sir fihalatk`;
+}
